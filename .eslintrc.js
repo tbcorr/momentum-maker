da0ce9a67eae766c8fd826175ff540e379727a76
Tim Corr <tbcorr@timothys-mbp.myfiosgateway.com>
2023-03-12 22:31:54 -0400

    chore: use editorconfig, eslint, prettier, commitizen, husky

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+export default {
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier-vue/recommended",
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
